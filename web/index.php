0c6d73ade8802fd8cf5558bff4b11ebf55029964
Abi Hafshin <abiehafshin@gmail.com>
2014-06-08 12:54:43 +0700

    Redesign bootstrap - custom config - config caching

--- web/index.php
+++ web/index.php
@@ -17,33 +17,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-
-//
-if (isset($_ENV['DATABASE_URL']) && preg_match('#postgres://(\w+):(\w+)@([\w-.]+):(\d+)/(\w+)#', $_ENV['DATABASE_URL'], $m)) {
-    define('DB_DRIVER', 'pgsql');
-    define('DB_USER', $m[1]);
-    define('DB_PASSWORD', $m[2]);
-    define('DB_HOST', $m[3]);
-    define('DB_PORT', $m[4]);
-    define('DB_NAME', $m[5]);
-}
-elseif (file_exists($configFile  = __DIR__ . '/../config.php')) {
-    include $configFile;
-} else {
-    die ('config.php not found');
-}
-
-defined('YII_DEBUG') or define('YII_DEBUG',true);
-defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
-
-if (YII_DEBUG) {
-    ini_set('display_errors', 1);
-    error_reporting(E_ALL);
-}
-
-require_once(__DIR__ . '/../vendor/yiisoft/yii/framework/yii.php');
-require_once(__DIR__ . '/../protected/organizzy.php');
-
 if (isset($_SERVER['HTTP_ORIGIN'])) {
     header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
     header('Access-Control-Allow-Methods: GET, POST');
@@ -56,4 +29,9 @@ if (isset($_SERVER['HTTP_ORIGIN'])) {
         return;
     }
 }
-O::createOrganizzyApplication(__DIR__.'/../protected/config/main.php')->run();
+
+require_once(__DIR__ . '/../vendor/yiisoft/yii/framework/yii.php');
+require_once(__DIR__ . '/../protected/organizzy.php');
+
+$config = @include(__DIR__ . '/../config.php');
+O::createOrganizzyApplication($config)->run();
